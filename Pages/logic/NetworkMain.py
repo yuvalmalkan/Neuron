353335f1a6184ecd85d80d458758177e3e747f72
Malkan <yuvalmalkan89@gmail.com>
2026-04-10 22:28:42 +0300

    added NetworkMain.py

---
+++ Pages/logic/NetworkMain.py
@@ -0,0 +1,8 @@
+__author__ = "Yuval Malkan"
+
+import logging
+
+
+#todo here will be all the network page behind the scenes logic, such as functions, and sending messages to the UI, and
+# combining the gemini with the results.
+
