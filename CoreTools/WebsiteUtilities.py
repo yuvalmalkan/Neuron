8526cfc469b373101bd773435c379d40f4ecc883
Malkan <yuvalmalkan89@gmail.com>
2026-04-25 18:55:03 +0300

    first prototype

--- CoreTools/WebsiteUtilities.py
+++ CoreTools/WebsiteUtilities.py
@@ -1,5 +1,8 @@
 __author__ = "Yuval Malkan"
 
+import logging
+
+import requests
 
 
 def downloadHtml(url):
@@ -9,4 +12,26 @@ def downloadHtml(url):
     args: url
 
     """
-    pass
+
+    try:
+
+        response = requests.get(url)
+
+        # Check if the request was successful (Status Code 200)
+        response.raise_for_status()
+
+        html_content = response.text
+
+        logging.debug("Successfully downloaded source code!")
+        # print(html_content)
+
+        with open('page_source.html', 'w', encoding='utf-8') as file:
+            file.write(html_content)
+
+    except requests.exceptions.RequestException as e:
+        logging.debug(f"An error occurred: {e}")
+
+
+
+if __name__ == "__main__":
+    downloadHtml("https://www.instagram.com")
