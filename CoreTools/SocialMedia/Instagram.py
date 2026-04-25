a5ef124226caa4dfe169619767bd62ab0d3ef79f
Malkan <yuvalmalkan89@gmail.com>
2026-04-25 22:08:01 +0300

    added base dict

--- CoreTools/SocialMedia/Instagram.py
+++ CoreTools/SocialMedia/Instagram.py
@@ -9,15 +9,18 @@ def get_info_from_html(filePath: str) -> dict:
     return: profile info as dict, e.g. {"username": "yuvalmalkan", "full_name": "Yuval Malkan", "bio": "Software Engineer", ...}
     """
 
+    profile = {
+        "username": "",
+        "display_name": "",
+        "bio": "",
+        "profile_picture_url": "",
+        "followers": 0,
+        "following": 0,
+        "profile_url": "",
+        "number_of_posts": 0
+    }
 
-    username = ""
-    display_name = ""
-    bio = ""
-    profile_picture_url = ""
-    followers = 0
-    following = 0
-    profile_url = ""
-    number_of_posts = 0
+
 
     with open(filePath, "r", encoding="utf-8") as data:
         data = data.read()
