a3cd097e97fb19589b30185cc07995ac52618d69
Malkan <yuvalmalkan89@gmail.com>
2026-04-25 22:01:39 +0300

    added function documentation and base

---
+++ CoreTools/SocialMedia/Instagram.py
@@ -0,0 +1,33 @@
+__author__ = 'Yuval Malkan'
+
+
+def get_info_from_html(filePath: str) -> dict:
+    """
+    Get profile info from html file,
+
+    args: html file path
+    return: profile info as dict, e.g. {"username": "yuvalmalkan", "full_name": "Yuval Malkan", "bio": "Software Engineer", ...}
+    """
+
+
+    username = ""
+    display_name = ""
+    bio = ""
+    profile_picture_url = ""
+    followers = 0
+    following = 0
+    profile_url = ""
+    number_of_posts = 0
+
+    with open(filePath, "r", encoding="utf-8") as data:
+        data = data.read()
+
+
+
+
+
+
+
+
+
+
