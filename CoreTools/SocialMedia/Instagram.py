ced2ec2e7ef21ed158f9e3404ffe05174852055f
Malkan <yuvalmalkan89@gmail.com>
2026-04-26 17:12:57 +0300

    fixed display_name search

--- CoreTools/SocialMedia/Instagram.py
+++ CoreTools/SocialMedia/Instagram.py
@@ -46,10 +46,14 @@ def get_info_from_html(filename: str) -> dict:
 
 
 
-        #fixme remove "See Instagram photos and videos from "
+
         display_name = re.search(r'Posts\s+-\s+([^(]+?)\s+\(&#064;', data)
         if display_name:
-            profile["display_name"] = display_name.group(1).strip()
+            display_name = display_name.group(1).strip()
+            textToRemove = "See Instagram photos and videos from "
+            display_name = display_name[len(textToRemove)::]
+            profile["display_name"] = display_name
+
 
 
 
