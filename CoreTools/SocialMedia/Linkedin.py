f71fb43e6d2309294c9e1a0ec5f1d3895d038d25
Malkan <yuvalmalkan89@gmail.com>
2026-05-03 19:29:14 +0300

    cleanup

--- CoreTools/SocialMedia/Linkedin.py
+++ CoreTools/SocialMedia/Linkedin.py
@@ -1,7 +1,5 @@
 __author__ = "Yuval Malkan"
 
-#cant scrape linkedin without login, and login requires captcha, so i will not implement it for now
-
 from playwright.sync_api import sync_playwright
 import re
 import logging
