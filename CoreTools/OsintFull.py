a47dbce6ccd58cd2edf912689952c9156e4993ab
Malkan <yuvalmalkan89@gmail.com>
2026-05-08 20:49:04 +0300

    added documentation

--- CoreTools/OsintFull.py
+++ CoreTools/OsintFull.py
@@ -5,9 +5,18 @@ import Maigret
 import Sherlock
 
 
+
+"""
+here the full osint scan will happen in one big function that will call all the different tools and combine the results into one report.
+every tool will be called on a different thread or using async 
+
+eventually all of the data will be fed into gemini to create a final report that will be more human readable and will include all the relevant information about the user, 
+including links to their profiles on different platforms, their activity, and any other relevant information that was found during the scan.
+
+"""
+
+
 x = Sherlock.search_username("yuvalmalkan")
 
 
 
-
-#todo download html from user profile page and extract information locally
