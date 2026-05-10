6eb56797181e52b96a9996f6e61f5edd2be25bc1
Malkan <yuvalmalkan89@gmail.com>
2026-05-10 14:07:48 +0300

    added Submit_OTP_btn

--- Pages/ui/ForgotPassword.py
+++ Pages/ui/ForgotPassword.py
@@ -28,12 +28,18 @@ class ForgotPasswordForm(QWidget):
 
         self.OTP_input = GlowInput("Password Reset Code")
 
+
+        self.Submit_OTP_btn = GlowingButton("SUBMIT CODE", "primary")
+
+
+
         self.switch_btn = GlowingButton("SWITCH BACK TO LOGIN", "danger")
 
 
 
         self.switch_btn.clicked.connect(lambda: self.window().show_login())
         self.sendOTP_btn.clicked.connect(lambda: HandleForgotPassword(self))
+        #todo connect Submit_OTP_btn to function
 
 
 
@@ -41,12 +47,14 @@ class ForgotPasswordForm(QWidget):
         layout.addSpacing(30)
         layout.addWidget(self.sendOTP_btn)
         layout.addWidget(self.OTP_input)
+        layout.addWidget(self.Submit_OTP_btn)
         layout.addStretch()
 
         layout.addWidget(self.switch_btn)
 
 
         #todo make it so when the send code is clicked a text will appear saying "Verification code sent to email" and then show the input fields for the code and new password, and a button to submit the new password
+
 
 
 
